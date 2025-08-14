106a020b8041159c288bbec3c06afefed511fc65
DinhTQD <dinh.tqd.acc@gmail.com>
2025-08-14 20:20:27 +0700

    14/08 20:20

--- src/chap13/ex1304/TestEx1304.java
+++ src/chap13/ex1304/TestEx1304.java
@@ -2,28 +2,28 @@ package chap13.ex1304;
 
 public class TestEx1304 {
     public static void main(String[] args) {
-        // Test trường hợp 1: Chỉ có 1 tham số (tháng)
-        System.out.println("Test với 8");
+        // Case 1: 1 parameter (month)
+        System.out.println("Test with 8");
         String[] argsOneParam = {"8"}; // Tháng 8
         Ex1304.main(argsOneParam);
-        System.out.println("--------------------------------");
+        System.out.print("--------------------------------\n\n\n\n");
 
         // Case 2: 2 parameters
         System.out.println("Test with 12 2025");
         String[] argsTwoParams = {"12", "2025"}; // December 2025
         Ex1304.main(argsTwoParams);
-        System.out.println("--------------------------------");
+        System.out.print("--------------------------------\n\n\n\n");
 
         // Case 3: No parameters
         System.out.println("Test with no parameters");
         String[] argsNoParam = {};
         Ex1304.main(argsNoParam);
-        System.out.println("--------------------------------");
+        System.out.print("--------------------------------\n\n\n\n");
 
         // Case 4: 3 parameters
         System.out.println("Test with 10 2025 8");
         String[] args3Param = {"10", "2025", "8"};
         Ex1304.main(args3Param);
-        System.out.println("--------------------------------");
+        System.out.print("--------------------------------\n\n\n\n");
     }
 }
