abaf69f6f4d9debe3cc4a9fef5e063718567013a
DinhTQD <dinh.tqd.acc@gmail.com>
2025-08-14 08:16:30 +0700

    Fix Ex1312

--- src/chap13/ex1312/Ex1312.java
+++ src/chap13/ex1312/Ex1312.java
@@ -2,10 +2,15 @@ package chap13.ex1312;
 
 public class Ex1312 {
     public static void main(String[] args) {
-        GeometricObject[] array = {new Circle(2), new Octagon(4), new Rectangle(2,4) };
+        GeometricObject[] array = {
+                new Circle(2),
+                new Circle(),
+                new Rectangle(),
+                new Rectangle(2,4) };
         System.out.println(array[0].getArea());
         System.out.println(array[1].getArea());
         System.out.println(array[2].getArea());
+        System.out.println(array[3].getArea());
         System.out.println(sumArea(array));
     }
 
